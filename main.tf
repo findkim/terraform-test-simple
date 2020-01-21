ba29ee6c460b4b34a10f791e83324bc5a4c46409
Kim Ngo <akngo16@gmail.com>
2020-01-21 14:55:50 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,16 @@
 resource "null_resource" "test" {}
+
+provider "template" {}
+
+provider "gooddocs" {}
+
+terraform {
+  required_providers {
+    null = {
+      version = ">= 2.0.0"
+    }
+    gooddocs = {
+      source = "findkim/gooddocs"
+    }
+  }
+}
